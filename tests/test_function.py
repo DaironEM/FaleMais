6950bf6baf4f09078c58c31b44f9d23bf9e684f2
Dairon <dairon10_@hotmail.com>
2020-11-13 21:35:28 -0300

    Correct the names of the tests

--- tests/test_function.py
+++ tests/test_function.py
@@ -1,14 +1,14 @@
 import pytest
 from function import fill_area_code
 
-def test_entrada_de_um_digito_é_corrigida():
+def test_if_one_digit_entry_is_acceptable():
     assert fill_area_code('1') == '001'
 
-def test_entrada_de_dois_digitos_é_corrigida():
+def test_if_two_digit_entry_is_acceptable():
     assert fill_area_code('11') == '011'
 
-def test_entrada_de_tres_digitos_é_corrigida():
+def test_if_three_digit_entry_is_acceptable():
     assert fill_area_code('011') == '011'
 
-def test_entrada_de_mais_de_tres_digitos_é_corrigida():
+def test_if_a_highter_digit_entry_is_acceptable():
     assert fill_area_code('0000011') == '011'
--- tests/test_talk_more.py
+++ tests/test_talk_more.py
@@ -18,42 +18,42 @@ def third_case():
 def fourth_case():
     return TalkMore('018', '017', 100, 'FaleMais 30')
 
-def test_se_a_tarifa_esta_sendo_calculada_corretamente(first_case):
+def test_if_tariff_is_being_computed_correctly(first_case):
     first_case.cost_per_minute()
     assert first_case.tariff == 1.9
 
-def test_se_tarifas_validas_estao_sendo_tratadas_corretamente(first_case):
+def test_if_valid_tariff_are_beimg_rightly_precessed(first_case):
     assert first_case.is_valid_call() == True
 
-def test_se_tarifas_invalidas_estao_sendo_tratadas_corretamente(fourth_case):
+def test_if_invalid_tariff_are_beimg_rightly_precessed(fourth_case):
     assert fourth_case.is_valid_call() == False
 
-def test_se_o_calculo_custo_esta_sendo_feito_corretamente(first_case):
+def test_if_cost_timeis_being_correctly_rsolved(first_case):
     assert first_case.cost_time_without_plan() == 38
 
-def test_se_franquia_esta_sendo_calculada_corretamente(first_case):
+def test_if_free_time_of_plan_is_correct(first_case):
     assert first_case.free_time_for_plan() == 30
 
-def test_se_valor_total_com_plano_esta_sendo_calculado_corretamente(first_case):
+def test_if_total_cost_with_plan_is_being_rightly_computed_first_case(first_case):
     assert first_case.cost_of_call_with_plan() == '$ 0.00'
 
-def test_se_valor_total_sem_plano_esta_sendo_calculado_corretamente(first_case):
+def test_if_total_cost_without_plan_is_being_rightly_computed_first_case(first_case):
     assert first_case.cost_of_call_without_plan() == '$ 38.00'
 
-def test_se_valor_total_com_plano_esta_sendo_calculado_corretamente_segundo_caso(second_case):
+def test_if_total_cost_with_plan_is_being_rightly_computed_second_case(second_case):
     assert second_case.cost_of_call_with_plan() == '$ 37.40'
 
-def test_se_valor_total_sem_plano_esta_sendo_calculado_corretamente_segundo_caso(second_case):
+def test_if_total_cost_without_plan_is_being_rightly_computed_second_case(second_case):
     assert second_case.cost_of_call_without_plan() == '$ 136.00'
 
-def test_se_valor_total_com_plano_esta_sendo_calculado_corretamente_segundo_caso(third_case):
+def test_if_total_cost_with_plan_is_being_rightly_computed_third_case(third_case):
     assert third_case.cost_of_call_with_plan() == '$ 167.20'
 
-def test_se_valor_total_sem_plano_esta_sendo_calculado_corretamente_segundo_caso(third_case):
+def test_if_total_cost_without_plan_is_being_rightly_computed_third_case(third_case):
     assert third_case.cost_of_call_without_plan() == '$ 380.00'
 
-def test_se_valor_total_com_plano_esta_sendo_calculado_corretamente_caso_ligacao_ilegal_com_plano(fourth_case):
+def test_if_total_cost_with_plan_is_being_rightly_computed_with_inlegal_code(fourth_case):
     assert fourth_case.cost_of_call_with_plan() == '-'
 
-def test_se_valor_total_com_plano_esta_sendo_calculado_corretamente_caso_ligacao_ilegal_sem_plano(fourth_case):
+def test_if_total_cost_without_plan_is_being_rightly_computed_with_inlegal_code(fourth_case):
     assert fourth_case.cost_of_call_without_plan() == '-'
