553e78f83f626298fb2a929c2edc0862c9a91792
Dairon <dairon10_@hotmail.com>
2020-11-13 22:01:02 -0300

    ADD tests to the new function

--- tests/test_function.py
+++ tests/test_function.py
@@ -1,5 +1,7 @@
 import pytest
-from function import fill_area_code
+from function import fill_area_code, correct_lenght
+
+#tests for fill_area_code
 
 def test_if_one_digit_entry_is_acceptable():
     assert fill_area_code('1') == '001'
@@ -12,3 +14,14 @@ def test_if_three_digit_entry_is_acceptable():
 
 def test_if_a_highter_digit_entry_is_acceptable():
     assert fill_area_code('0000011') == '011'
+    
+#tests for correct_lenght
+
+def test_if_empty_lenght_is_acceptable():
+    assert correct_lenght('') == 0
+
+def test_if_negative_minutes_is_acceptable():
+    assert correct_lenght('-21') == 0
+
+def test_if_string_is_being_correctly_converted():
+    assert correct_lenght('23.4') == 23.4
